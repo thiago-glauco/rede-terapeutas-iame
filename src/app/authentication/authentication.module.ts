ffe88d8555ca7768640140a794a8c5fa52a88298
thiago-glauco <thiagoglauco@yahoo.com.br>
2020-02-07 11:07:07 -0300

    2 files modified

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './register/register.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 
 import { MatIconModule } from '@angular/material/icon';
@@ -24,7 +23,6 @@ import { AuthRootComponent } from './auth-root/auth-root.component';
 @NgModule({
   imports: [
     CommonModule,
-    BrowserAnimationsModule,
     AuthenticationRoutingModule,
     FormsModule,
     MatIconModule,
